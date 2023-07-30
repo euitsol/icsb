7bc4073e470e173c9fe425934e898652d2c9574a
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-07-30 15:54:11 +0600

    Single pages seeder --done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             UsersSeeder::class,
-            SiteSettingsSeeder::class
+            SiteSettingsSeeder::class,
+            SinglePagesSeeder::class
         ]);
     }
 }
