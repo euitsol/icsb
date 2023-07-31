72a3d150be1f336d06e0d9fe8b75ef291102778f
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-07-31 17:21:24 +0600

    mission and vision --done

--- resources/views/frontend/about/vision.blade.php
+++ resources/views/frontend/about/vision.blade.php
@@ -5,25 +5,28 @@
 @section('content')
 <section class="breadcrumbs-section">
 	<div class="overly-image">
+        @if(!empty($single_page) && !empty(json_decode($single_page->saved_data)) && isset(json_decode($single_page->saved_data)->{"banner-image"}))
 		<img src='{{storage_url(json_decode($single_page->saved_data)->{"banner-image"})}}' alt="Vision & Mission">
+        @endif
 	</div>
 	<div class="container">
 		<div class="breadcrumbs-row flex">
 		<div class="left-column content-column">
 			<div class="inner-column color-white">
-				<h1 class="breadcrumbs-heading">Vision & Mission</h1>
+				<h1 class="breadcrumbs-heading">Vision</h1>
 				<ul class="flex">
 					<li><a href="index">Home</a></li>
 					<li><i class="fa-solid fa-angle-right"></i></li>
 					<li><a href="#">About ICSB</a></li>
 					<li><i class="fa-solid fa-angle-right"></i></li>
-					<li><p>Vision & Mission</p></li>
+					<li><p>Vision</p></li>
 				</ul>
 			</div>
 		</div>
 	</div>
 	</div>
 </section>
+@if(!empty(json_decode($single_page->saved_data)) && isset(json_decode($single_page->saved_data)->{'image'}) && isset(json_decode($single_page->saved_data)->{'details'}))
 <section class="mision-vision-section">
     <div class="container">
         <div class="mission-row flex">
@@ -36,6 +39,7 @@
         </div>
     </div>
 </section>
+@endif
 
 @include('frontend.includes.national_awards',['national_awards'=>$national_awards])
 
