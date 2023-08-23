57ab6fc157111fc67e14995f18e5dad85bafdcc1
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-08-23 14:57:54 +0600

    Enroll section update

--- resources/views/frontend/includes/endorsement.blade.php
+++ resources/views/frontend/includes/endorsement.blade.php
@@ -7,7 +7,7 @@
                 <div class="reg-field-wrapp">
                     <div class="content">
                         <div class="heading">
-                            <h3>Admit as a Student</h3>
+                            <h3>Admit as a <br> Student</h3>
                         </div>
                         <div class="button">
 
@@ -61,6 +61,28 @@
                         <img
                             class="object-fit-cover"
                             src="{{ asset('frontend/img/endorsement/Untitled-1.png') }}"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div
+                class="endorsement-endor-column new endorsement-column"
+            >
+                <div class="reg-field-wrapp">
+                    <div class="content">
+                        <div class="heading">
+                            <h3>
+                                Find a corporate leader
+                            </h3>
+                        </div>
+                        <div class="button">
+                            <a href="#" class="text-align">Find</a>
+                        </div>
+                    </div>
+                    <div class="bg-image">
+                        <img
+                            class="object-fit-cover"
+                            src="{{ asset('frontend/img/endorsement/new.png') }}"
                         />
                     </div>
                 </div>
