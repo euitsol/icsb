93ddc8e38fdaf1525e02ca5f941b5ae7f50f22e4
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-12 20:41:58 +0600

    Update

--- resources/views/frontend/publication/the_cs.blade.php
+++ resources/views/frontend/publication/the_cs.blade.php
@@ -30,7 +30,7 @@
                 @php
                     $files = array_reverse((array)json_decode($single_page->saved_data)->{'upload-files'});
                 @endphp
-                    @foreach (array_slice($files, -1) as $file)
+                    @foreach (array_slice($files, -12) as $file)
                         <div class="col-md-3 the_cs mb-5">
                             <div class="new-handbook text-align">
                                     <iframe src="{{ route('view.pdf', base64_encode($file)) }}" type="application/pdf" width="100%" height="200px"></iframe>
