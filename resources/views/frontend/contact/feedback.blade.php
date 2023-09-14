9b9e1ae788ee498491ece5b573a93d238291d531
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-14 19:05:08 +0600

    Feedback design update

--- resources/views/frontend/contact/feedback.blade.php
+++ resources/views/frontend/contact/feedback.blade.php
@@ -22,17 +22,20 @@ $datas = [
     <div class="container">
         <div class="form-content">
             <div class="form-column">
-                <h2>Fill The Form Below</h2>
-                <form>
-                    <input type="text" name="name" placeholder="Name:">
-                    <input type="email" name="email" placeholder="Email:">
-                    <input type="tel" name="phone" placeholder="Phone Number:">
-                    <input type="text" name="subject" placeholder="Your Subject:">
-                    <textarea placeholder="Your Message Here:"></textarea>
-                    <!-- <div class="g-recaptcha" data-sitekey="6Lfk7gkkAAAAAP3zBcc1gT8AeJVEI-l0lXeCpk7H"></div> -->
-                    <!-- <button class="g-recaptcha" data-sitekey="reCAPTCHA_site_key" data-callback='onSubmit'data-action='submit'>Submit</button> -->
-                    <input class="submit-button" type="submit" name="submit" value="Submit Now" class="g-recaptcha" data-sitekey="reCAPTCHA_site_key" data-callback='onSubmit'data-action='submit'>
-                </form>
+                <div class="form-border">
+                    <h2>Fill The Form Below</h2>
+                    <form>
+                        <input type="text" value="To:CS Bangladesh" disabled>
+                        <input type="text" name="name" placeholder="Your Name:">
+                        <input type="email" name="email" placeholder="Your Email:">
+                        <input type="tel" name="phone" placeholder="Your Phone Number:">
+                        <input type="text" name="subject" placeholder="Your Subject:">
+                        <textarea placeholder="Your Message Here:"></textarea>
+                        <!-- <div class="g-recaptcha" data-sitekey="6Lfk7gkkAAAAAP3zBcc1gT8AeJVEI-l0lXeCpk7H"></div> -->
+                        <!-- <button class="g-recaptcha" data-sitekey="reCAPTCHA_site_key" data-callback='onSubmit'data-action='submit'>Submit</button> -->
+                        <input class="submit-button" type="submit" name="submit" value="Submit Now" class="g-recaptcha" data-sitekey="reCAPTCHA_site_key" data-callback='onSubmit'data-action='submit'>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
