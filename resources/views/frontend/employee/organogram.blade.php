748bf505cb705a5260971a53e88fbd11a57a5fd7
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-16 19:07:59 +0600

    Update Organogram

--- resources/views/frontend/employee/organogram.blade.php
+++ resources/views/frontend/employee/organogram.blade.php
@@ -18,30 +18,39 @@ $datas = [
 @endphp
 @include('frontend.includes.breadcrumb',['datas'=>$datas])
 <!-- =============================== Breadcrumb Section ======================================-->
-    <section class="organogram-section big-sec-height">
+    <section class="organogram-section big-sec-min-height">
         <div class="container">
             <div class="chart-main">
             <div class="level-1">
-                <div class="box">
-                <h2>President</h2>
-                </div>
+                @foreach ($councils as $key=>$council)
+                    @if($key == count($councils)-1)
+                        <a href="{{route('council_view.council.members',$council->slug)}}">
+                            <div class="box box-0">
+                                <h2>{{_('Council')}}</h2>
+                            </div>
+                        </a>
+                    @endif
+                @endforeach
+
             </div>
             <div class="level-1">
-                <div class="box">
-                <h2>Secretary & CEO</h2>
-                </div>
+                <a href="{{route('employee_view.sec_and_ceo')}}">
+                    <div class="box box-1">
+                        <h2>{{_('Secretary & CEO')}}</h2>
+                    </div>
+                </a>
             </div>
             <div class="level-2">
-                <div class="box d-flex align-items-center justify-content-center"><h3>HR & Admin Dept.</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Education Department</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Accounts & Finance Department</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Library</h3></div>
+                <div class="box box-2 d-flex align-items-center justify-content-center"><h3>{{_('HR & Admin Dept.')}}</h3></div>
+                <div class="box box-3 d-flex align-items-center justify-content-center"><h3>{{_('Education Department')}}</h3></div>
+                <div class="box box-4 d-flex align-items-center justify-content-center"><h3>{{_('Accounts & Finance Department')}}</h3></div>
+                <div class="box box-5 d-flex align-items-center justify-content-center"><h3>{{_('Library')}}</h3></div>
             </div>
             <div class="level-3">
-                <div class="box d-flex align-items-center justify-content-center"><h3>Professional Training Department</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Exam Department</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Publication Research & Development</h3></div>
-                <div class="box d-flex align-items-center justify-content-center"><h3>Membership, Registation & Management Department</h3></div>
+                <div class="box box-6 d-flex align-items-center justify-content-center"><h3>{{_('Professional Training Department')}}</h3></div>
+                <div class="box box-7 d-flex align-items-center justify-content-center"><h3>{{_('Exam Department')}}</h3></div>
+                <div class="box box-8 d-flex align-items-center justify-content-center"><h3>{{_('Publication Research & Development')}}</h3></div>
+                <div class="box box-9 d-flex align-items-center justify-content-center"><h3>{{_('Membership, Registation & Management Department')}}</h3></div>
             </div>
             </div>
         </div>
