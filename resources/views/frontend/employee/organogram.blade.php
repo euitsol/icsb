3622dd15152d947f699eda186d6320bb8d0e8b4f
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-17 15:29:54 +0600

    Upsate Organogram & Media room Design

--- resources/views/frontend/employee/organogram.blade.php
+++ resources/views/frontend/employee/organogram.blade.php
@@ -36,12 +36,12 @@ $datas = [
             <div class="level-1">
                 <a href="{{route('employee_view.sec_and_ceo')}}">
                     <div class="box box-1">
-                        <h2>{{_('Secretary & CEO')}}</h2>
+                        <h2>{{_("Secretary")}} <br> {{_("& CEO")}}</h2>
                     </div>
                 </a>
             </div>
             <div class="level-2">
-                <div class="box box-2 d-flex align-items-center justify-content-center"><h3>{{_('HR & Admin Dept.')}}</h3></div>
+                <div class="box box-2 d-flex align-items-center justify-content-center"><h3>{{_('HR & Admin Department')}}</h3></div>
                 <div class="box box-3 d-flex align-items-center justify-content-center"><h3>{{_('Education Department')}}</h3></div>
                 <div class="box box-4 d-flex align-items-center justify-content-center"><h3>{{_('Accounts & Finance Department')}}</h3></div>
                 <div class="box box-5 d-flex align-items-center justify-content-center"><h3>{{_('Library')}}</h3></div>
