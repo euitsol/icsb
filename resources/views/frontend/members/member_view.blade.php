3e77343a4d3dd86dae81baee0b72612e3237dda3
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-23 16:46:30 +0600

    Updated Member search jequery

--- resources/views/frontend/members/member_view.blade.php
+++ resources/views/frontend/members/member_view.blade.php
@@ -33,6 +33,22 @@ $datas = [
             </div>
         </div>
         <div class="fellow-row flex member_data">
+            @if($type->id ==3 || $type->id ==4)
+                @foreach ($type->members as $member)
+                    <div class="fellow-items flex">
+                        <div class="image-column">
+                            <img src="{{getMemberImage($member)}}" alt="{{$member->name}}">
+                        </div>
+                        <div class="content-column">
+                            <h4>{{$member->membership_id}}</h4>
+                            <h3 class="mb-0">{{$member->name}}</h3>
+                            <p><strong>{{$member->designation}}</strong></p>
+                            <li><i class="fa-solid fa-house-circle-exclamation"></i>{{$member->address}}</li>
+                            <li><i class="fa-solid fa-envelope-open-text"></i>{{_('Email:')}} <a href="mailto:{{$member->email}}">{{$member->email}}</a></li>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 </section>
@@ -40,7 +56,56 @@ $datas = [
 @push('js')
     <script>
         $(document).ready(function() {
-            $('.search_button').on('click', function() {
+            // $('.search_button').on('click', function() {
+            //     let search_value = $('.search_value').val();
+            //     if (search_value != null) {
+            //         let _url = ("{{ route('member_info.search', ['search_value','cat_id']) }}");
+            //         let __url = _url.replace('search_value', search_value);
+            //         let ___url = __url.replace('cat_id', {{$type->id}});
+            //         console.log(___url);
+
+            //         $.ajax({
+            //             url: ___url,
+            //             method: 'GET',
+            //             dataType: 'json',
+            //             success: function(data) {
+            //                 console.log(data);
+            //                 var member_data= '';
+            //                 if(!data.member_searchs || data.member_searchs.length === 0){
+            //                     console.log(data);
+            //                     member_data +=`
+            //                                     <h3 class="text-danger mx-auto my-5">Member Not Found</h3>
+            //                                 `;
+            //                 } else{
+
+            //                     data.member_searchs.forEach(function(member_search) {
+            //                         member_data += `
+            //                             <div class="fellow-items flex">
+            //                                 <div class="image-column">
+            //                                     <img src="${member_search.image}" alt="">
+            //                                 </div>
+            //                                 <div class="content-column">
+            //                                     <h4>${member_search.membership_id}</h4>
+            //                                     <h3 class="mb-0">${member_search.name}</h3>
+            //                                     <p><strong>${member_search.designation}</strong></p>
+            //                                     <li><i class="fa-solid fa-house-circle-exclamation"></i>${member_search.address}</li>
+            //                                     <li><i class="fa-solid fa-envelope-open-text"></i>Email: <a href="mailto:${member_search.email}">${member_search.email}</a></li>
+            //                                 </div>
+            //                             </div>
+            //                         `;
+            //                     });
+
+            //                 }
+            //                 $('.member_data').html(member_data);
+
+            //             },
+            //             error: function(xhr, status, error) {
+            //                 console.error('Error fetching member data:', error);
+            //             }
+            //         });
+            //     }
+            // });
+            $('.search_value').on('keyup', function() {
                 let search_value = $('.search_value').val();
                 if (search_value != null) {
                     let _url = ("{{ route('member_info.search', ['search_value','cat_id']) }}");
@@ -90,6 +155,5 @@ $datas = [
                 }
             });
         });
-
     </script>
 @endpush
