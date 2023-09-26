76ef8664becee2ceca905d4019b7a2e29e1f4849
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-26 21:55:30 +0600

    Notice Issue Done

--- app/Http/Controllers/Frontend/NoticeBoardPageController.php
+++ app/Http/Controllers/Frontend/NoticeBoardPageController.php
@@ -58,8 +58,8 @@ class NoticeBoardPageController extends Controller
 
         $s=[];
         if($slug != false){
-            $s['notice_cat'] = NoticeCategory::where('slug',$slug)->where('deleted_at',null)->first();
-            $s['notices'] = Notice::where('cat_id',$s['notice_cat']->id)->where('deleted_at',null)->where('status',1)->latest()->paginate(10);
+            // $s['notice_cat'] = NoticeCategory::where('slug',$slug)->where('deleted_at',null)->first();
+            $s['notices'] = Notice::where('slug',$slug)->where('deleted_at',null)->where('status',1)->latest()->paginate(10);
         }else{
             $s['notices'] = Notice::where('deleted_at',null)->where('status',1)->latest()->paginate(10);
         }
