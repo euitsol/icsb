e7a8f0a8d1ee2c240f2d588d8e4ca2fd73579de9
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-09-29 13:28:48 +0600

    Committee order frontend --done

--- app/Http/Controllers/Frontend/CouncilPagesController.php
+++ app/Http/Controllers/Frontend/CouncilPagesController.php
@@ -26,7 +26,11 @@ class CouncilPagesController extends Controller
     public function __construct() {
         $contact = Contact::where('deleted_at', null)->first();
         $memberTypes = MemberType::where('deleted_at', null)->where('status', 1)->orderBy('order_key','ASC')->get();
-        $committeeTypes = CommitteeType::with('committees')->where('deleted_at', null)->where('status', 1)->get();
+        $committeeTypes = CommitteeType::with('committees')->where('deleted_at', null)->where('status', 1)
+        ->whereHas('committees', function ($query){
+            $query->orderBy('order_key','ASC');
+        })
+        ->get();
         $mediaRoomCategory = MediaRoomCategory::with('media_rooms')->where('deleted_at', null)->where('status', 1)->get();
         $bsss = SecretarialStandard::where('deleted_at', null)->where('status', 1)->get();
         $memberPortal = SinglePages::where('frontend_slug', 'member-portal')->first();
@@ -65,6 +69,7 @@ class CouncilPagesController extends Controller
                         ->where('committee_id',$s['committee']->id)
                         ->where('status',1)
                         ->where('deleted_at',null)
+                        ->orderBy('order_key','ASC')
                         ->get();
         return view('frontend.council.committee',$s);
     }
