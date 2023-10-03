59174e6a8f8ccf26229a01b1624a3b0b161ca86f
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-10-03 19:42:54 +0600

    Job create form --updated

--- resources/views/frontend/members/job_create.blade.php
+++ resources/views/frontend/members/job_create.blade.php
@@ -1,7 +1,15 @@
 @extends('frontend.master')
 
 @section('title', 'Create Job Posting')
+@push('css')
+<style>
+    .ck-rounded-corners .ck.ck-editor__main>.ck-editor__editable,
+    .ck.ck-editor__main>.ck-editor__editable.ck-rounded-corners {
+    height: 15vh !important;
+}
+</style>
 
+@endpush
 @section('content')
   <!-- =============================== Breadcrumb Section ======================================-->
 @php
@@ -24,6 +32,117 @@ $datas = [
   <!----============================= Job Index Section ========================---->
 
   <div class="job-create-section big-sec-min-height d-flex align-items-center">
-        <h1 class="mx-auto">Job Create Form</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mx-auto">
+                    <div class="job_create_form p-4">
+                        <form action="" method="post" class="p-4">
+                            <h2>CREATE A JOB POSTING</h2>
+                            <div class="row">
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="job_position">Position Name</label>
+                                    <input type="text" name="job_position" id="job_position" placeholder="Job Position:" class="form-control py-3 px-3">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="company_name">Company Name</label>
+                                    <input type="text" name="company_name" placeholder="Company Name:" class="form-control py-3 px-3">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="vacancy">Number of Vacancy</label>
+                                    <input type="text" name="vacancy" id="vacancy" placeholder="Number of Vacancies:" class="form-control py-3 px-3">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <fieldset>
+                                        <legend><label for="job_type1">Nature of Job</label></legend>
+                                        <div class="form-check" id="job_type">
+                                            <label class="form-check-label ms-5" for="job_type1">
+                                                <input class="form-check-input" type="radio" name="job_type" id="job_type1" value="full_time">
+                                                Full Time
+                                            </label>
+                                            <label class="form-check-label ms-5" for="job_type2">
+                                                <input class="form-check-input" type="radio" name="job_type" id="job_type2" value="part_time">
+                                                Part Time
+                                            </label>
+                                            <label class="form-check-label ms-5" for="job_type3">
+                                                <input class="form-check-input" type="radio" name="job_type" id="job_type3" value="contractual">
+                                                Contractual
+                                            </label>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="jr">Job Responsibility</label>
+                                    <textarea name="job_responsibility" id="jr" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Job Responsibility:"></textarea>
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="er">Educational Requirements</label>
+                                    <input type="text" name="educational_requirement" id="er" class="form-control py-3 px-3" placeholder="MBA/ M.Sc/ BBA/ Masters">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="pr">Professional Requirements</label>
+                                        <input type="text" name="professional_requirement" id="pr" class="form-control py-3 px-3" placeholder="FCS/ ACS/ QCS/ Certificate Level Passed">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="er">Experience Requirements</label>
+                                    <div class="input-group">
+                                        <input type="number" name="professional_requirement" id="er" class="form-control py-3 px-3 w-75" placeholder="Enter Experience Requirements">
+                                        <input type="text" value="in Years" class="form-control py-3 px-3 bold text-center w-25" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="ar">Age Requirements</label>
+                                    <input type="number" name="age_requirement" id="ar" class="form-control py-3 px-3" placeholder="Age at most in years">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="adr">Additional Requirements</label>
+                                    <textarea name="additional_requirements" id="adr" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Additional Requirements:"></textarea>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="jl">Job Location</label>
+                                    <textarea name="job_location" id="jl" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Job Location:"></textarea>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="ob">Other Benefits</label>
+                                    <textarea name="other_benefits" id="ob" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Other Benefits:"></textarea>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="si">Special Instructions</label>
+                                    <textarea name="special_instractions" id="si" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Special Instructions:"></textarea>
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="ad">Application Deadline</label>
+                                    <input type="date" name="application_deadline" id="ad" class="form-control py-3 px-3">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="email">Email Address</label>
+                                    <input type="email" name="email" id="email" class="form-control py-3 px-3" placeholder="Enter Company Email Address:">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="url">Application URL</label>
+                                    <input type="url" name="url" id="url" class="form-control py-3 px-3" placeholder="Enter Application URL:">
+                                </div>
+                                <div class="form-group mb-3 col-md-6">
+                                    <label for="curl">Company Website</label>
+                                    <input type="url" name="company_url" id="curl" class="form-control py-3 px-3" placeholder="Enter Company URL:">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="ca">Company Address</label>
+                                    <textarea name="company_address" id="ca" cols="30" rows="5" class="form-control py-3 px-3" placeholder="Enter Company Address:"></textarea>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="cpnd">Contact Person Name & Designation</label>
+                                    <textarea name="contact_details" id="cpnd" cols="30" rows="5" class="form-control py-3 px-3" placeholder="ICSB Official will communicate with the person to authenticate the job posting. Being authentication, ICSB Official will go accept this job circular for live posting and email to the members of the institute."></textarea>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
   </div>
 @endsection
+@push('js_link')
+<script src="{{ asset('backend/ckeditor/build/ckeditor.js') }}"></script>
+<script src="{{ asset('backend/js/ckeditor.js') }}"></script>
+@endpush
