6014318420bd877d4b329eb777cc84d47d93141f
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-10-17 16:38:08 +0600

    Jobplacement desing issue resolved

--- resources/views/frontend/members/job_index.blade.php
+++ resources/views/frontend/members/job_index.blade.php
@@ -22,18 +22,24 @@ $datas = [
 
   <!----============================= Job Index Section ========================---->
 
-  <div class="job-index-section small-sec-height d-flex align-items-center">
+  <div class="job-index-section big-sec-height">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <div class="employee">
-                        <a href="{{route('member_view.jps')}}" class="rounded p-5 btn btn-outline-info w-100">{{_("I'm Employee")}}</a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    {{-- <h1><a href="{{route('member_view.job_edit',[1,'-1'])}}">edit</a></h1> --}}
-                    <div class="employeer">
-                        <a href="{{route('member_view.job_create')}}" class="rounded p-5 btn btn-outline-primary w-100">{{_("I'm Employer")}}</a>
+                <div class="col-md-12">
+                    <div class="wrap">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="employee">
+                                    <a href="{{route('member_view.jps')}}" class="rounded py-4 btn w-100">{{_("I'm Employee")}}</a>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                {{-- <h1><a href="{{route('member_view.job_edit',[1,'-1'])}}">edit</a></h1> --}}
+                                <div class="employeer">
+                                    <a href="{{route('member_view.job_create')}}" class="rounded py-4 btn w-100">{{_("I'm Employer")}}</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
