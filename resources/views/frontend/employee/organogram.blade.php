033ce247cf70ca9e7d46f7e5e4e7d27f9bc948d0
mrsharifull <mdsharifulislamm.info@gmail.com>
2023-10-25 07:45:36 -0700

    Responsive Update

--- resources/views/frontend/employee/organogram.blade.php
+++ resources/views/frontend/employee/organogram.blade.php
@@ -21,37 +21,37 @@ $datas = [
     <section class="organogram-section big-sec-min-height">
         <div class="container">
             <div class="chart-main">
-            <div class="level-1">
-                @foreach ($councils as $key=>$council)
-                    @if($key == count($councils)-1)
-                        <a href="{{route('council_view.council.members',$council->slug)}}">
-                            <div class="box box-0">
-                                <h2>{{_('Council')}}</h2>
-                            </div>
-                        </a>
-                    @endif
-                @endforeach
+                <div class="level-1">
+                    @foreach ($councils as $key=>$council)
+                        @if($key == count($councils)-1)
+                            <a href="{{route('council_view.council.members',$council->slug)}}">
+                                <div class="box box-0">
+                                    <h2>{{_('Council')}}</h2>
+                                </div>
+                            </a>
+                        @endif
+                    @endforeach
 
-            </div>
-            <div class="level-1">
-                <a href="{{route('employee_view.sec_and_ceo')}}">
-                    <div class="box box-1">
-                        <h2>{{_("Secretary")}} <br> {{_("& CEO")}}</h2>
-                    </div>
-                </a>
-            </div>
-            <div class="level-2">
-                <div class="box box-2 d-flex align-items-center justify-content-center"><h3>{{_('HR & Admin Department')}}</h3></div>
-                <div class="box box-3 d-flex align-items-center justify-content-center"><h3>{{_('Education Department')}}</h3></div>
-                <div class="box box-4 d-flex align-items-center justify-content-center"><h3>{{_('Accounts & Finance Department')}}</h3></div>
-                <div class="box box-5 d-flex align-items-center justify-content-center"><h3>{{_('Library')}}</h3></div>
-            </div>
-            <div class="level-3">
-                <div class="box box-6 d-flex align-items-center justify-content-center"><h3>{{_('Professional Training Department')}}</h3></div>
-                <div class="box box-7 d-flex align-items-center justify-content-center"><h3>{{_('Exam Department')}}</h3></div>
-                <div class="box box-8 d-flex align-items-center justify-content-center"><h3>{{_('Publication Research & Development')}}</h3></div>
-                <div class="box box-9 d-flex align-items-center justify-content-center"><h3>{{_('Membership, Registation & Management Department')}}</h3></div>
-            </div>
+                </div>
+                <div class="level-1">
+                    <a href="{{route('employee_view.sec_and_ceo')}}">
+                        <div class="box box-1">
+                            <h2>{{_("Secretary")}} <br> {{_("& CEO")}}</h2>
+                        </div>
+                    </a>
+                </div>
+                <div class="level-2">
+                    <div class="box box-2 d-flex align-items-center justify-content-center"><h3>{{_('HR & Admin Department')}}</h3></div>
+                    <div class="box box-3 d-flex align-items-center justify-content-center"><h3>{{_('Education Department')}}</h3></div>
+                    <div class="box box-4 d-flex align-items-center justify-content-center"><h3>{{_('Accounts & Finance Department')}}</h3></div>
+                    <div class="box box-5 d-flex align-items-center justify-content-center"><h3>{{_('Library')}}</h3></div>
+                </div>
+                <div class="level-3">
+                    <div class="box box-6 d-flex align-items-center justify-content-center"><h3>{{_('Professional Training Department')}}</h3></div>
+                    <div class="box box-7 d-flex align-items-center justify-content-center"><h3>{{_('Exam Department')}}</h3></div>
+                    <div class="box box-8 d-flex align-items-center justify-content-center"><h3>{{_('Publication Research & Development')}}</h3></div>
+                    <div class="box box-9 d-flex align-items-center justify-content-center"><h3>{{_('Membership, Registation & Management Department')}}</h3></div>
+                </div>
             </div>
         </div>
     </section>
