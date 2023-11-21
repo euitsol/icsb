02e84615210ffdf9da18e1f5eb9624d4626bbd09
Sandeep Chowdhury <euitsols.suprt@gmail.com>
2023-11-21 17:54:55 +0600

    Update master.blade.php

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="cache-control" content="no-cache">
     <meta http-equiv="expires" content="0">
     <meta http-equiv="pragma" content="no-cache">
-    </script>
 
 	<title>
         CS Bangladesh
