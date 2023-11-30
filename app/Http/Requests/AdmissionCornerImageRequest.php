70c846b87645ea40a63bd94f2d0b6fb2001ac70d
mrsharifull <mdsharifulislamm.info@gmail.com>
2023-11-30 19:12:32 +0600

    Update Admission corner index page

--- app/Http/Requests/AdmissionCornerImageRequest.php
+++ app/Http/Requests/AdmissionCornerImageRequest.php
@@ -16,7 +16,7 @@ class AdmissionCornerImageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'page_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'page_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048|dimensions:max_width=1920,max_height=700,min_width=1920,min_height=700',
         ]
         +
         ($this->isMethod('POST') ? $this->store() : $this->update());
