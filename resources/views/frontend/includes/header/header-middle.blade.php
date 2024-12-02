4bc1937b752f23e61f4e01ba0c53aeb49d43551a
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-12-02 16:57:25 +0600

    Site title update

--- resources/views/frontend/includes/header/header-middle.blade.php
+++ resources/views/frontend/includes/header/header-middle.blade.php
@@ -3,11 +3,12 @@
         <div class="main-header-column">
             <div class="header-content text-align">
                 <div class="logo-column">
-                   <img src="{{storage_url(settings('site_logo'))}}">
+                    <img src="{{ storage_url(settings('site_logo')) }}">
                 </div>
-                <h1>{{settings('site_name')}}</h1>
-                <h2>A Statutory Body under an Act of Parliament</h2>
-                <p><small>Administrative Ministry: Ministry of Commerce, Government of the People's Republic of Bangladesh</small></p>
+                <h1>{{ settings('site_name') }}</h1>
+                <h2>A Statutory Body Under an Act of Parliament</h2>
+                <p><small>Administrative Ministry: Ministry of Commerce, Government of the People's Republic of
+                        Bangladesh</small></p>
             </div>
         </div>
     </div>
