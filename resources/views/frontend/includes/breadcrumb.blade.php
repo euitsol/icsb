7b2cc634657cc55e2706f730ea992058dc5075f0
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-09-01 14:22:43 +0600

    Update breadcrumb.blade.php

--- resources/views/frontend/includes/breadcrumb.blade.php
+++ resources/views/frontend/includes/breadcrumb.blade.php
@@ -7,13 +7,13 @@
 		<div class="breadcrumbs-row flex">
 		<div class="left-column content-column">
 			<div class="inner-column color-white">
-				<h1 class="breadcrumbs-heading" title="{{$datas['full_title'] ? $datas['full_title']: $datas['title']}}">{{$datas['title']}}</h1>
+				<h1 class="breadcrumbs-heading" title="{{ isset($datas['full_title']) ? $datas['full_title']: $datas['title']}}">{{$datas['title']}}</h1>
 				<ul class="flex">
                     @foreach ($datas['paths'] as $route=>$path)
                         <li title="{{$path}}"><a href='{{($route != 'javascript:void(0)') ? route($route) : $route}}'>{{$path}}</a></li>
                         <li><i class="fa-solid fa-angle-right"></i></li>
                     @endforeach
-					<li title="{{$datas['full_title'] ? $datas['full_title']: $datas['title']}}">{{$datas['title']}}</li>
+					<li title="{{ isset($datas['full_title']) ? $datas['full_title']: $datas['title']}}">{{$datas['title']}}</li>
 				</ul>
 			</div>
 		</div>
