30424593c016261cdb5014b31e6b749253fddd3b
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-09-01 14:13:50 +0600

    Add title attributes to breadcrumb elements
    Added title attributes to the breadcrumb heading and list items to improve accessibility and provide additional context on hover.

--- resources/views/frontend/includes/breadcrumb.blade.php
+++ resources/views/frontend/includes/breadcrumb.blade.php
@@ -7,13 +7,13 @@
 		<div class="breadcrumbs-row flex">
 		<div class="left-column content-column">
 			<div class="inner-column color-white">
-				<h1 class="breadcrumbs-heading">{{$datas['title']}}</h1>
+				<h1 class="breadcrumbs-heading" title="{{$datas['title']}}">{{$datas['title']}}</h1>
 				<ul class="flex">
                     @foreach ($datas['paths'] as $route=>$path)
-                        <li><a href='{{($route != 'javascript:void(0)') ? route($route) : $route}}'>{{$path}}</a></li>
+                        <li title="{{$path}}"><a href='{{($route != 'javascript:void(0)') ? route($route) : $route}}'>{{$path}}</a></li>
                         <li><i class="fa-solid fa-angle-right"></i></li>
                     @endforeach
-					<li>{{$datas['title']}}</li>
+					<li title="{{$datas['title']}}">{{$datas['title']}}</li>
 				</ul>
 			</div>
 		</div>
